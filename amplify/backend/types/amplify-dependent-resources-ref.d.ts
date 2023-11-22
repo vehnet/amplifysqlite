7b4e8e77852ab8613c9a0e90e4e350be1e58e20a
Jaume Sancho <jaume.sancho@vehnet.com>
2023-11-22 10:16:57 +0100

    created Amplify project with Vite

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifysqlite": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
